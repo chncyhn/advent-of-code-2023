8f38e652146bf0d52d400e9ec1e1917313335bc6
Cihan <chncyhn@gmail.com>
2024-02-11 21:05:25 +0100

    Add day 15

--- day15/day15.go
+++ day15/day15.go
@@ -3,20 +3,40 @@ package main
 import (
 	"bufio"
 	"os"
+	"strconv"
 	"strings"
 )
 
+type Command struct {
+	label string
+	op    string
+	value int
+}
+
+type LabelAndValue struct {
+	label string
+	value int
+}
+
 func hash(input string) (h int) {
-	/*
-		Determine the ASCII code for the current character of the string.
-		Increase the current value by the ASCII code you just determined.
-		Set the current value to itself multiplied by 17.
-		Set the current value to the remainder of dividing itself by 256.
-	*/
 	for _, c := range input {
 		h = int(c) + h
 		h = h * 17
 		h = h % 256
+	}
+	return
+}
+
+func parseCommands(inputs []string) (cmds []Command) {
+	for _, input := range inputs {
+		i := strings.Index(input, "=")
+		if i == -1 {
+			i = strings.Index(input, "-")
+			cmds = append(cmds, Command{label: input[:i], op: "-"})
+		} else {
+			value, _ := strconv.Atoi(input[i+1:])
+			cmds = append(cmds, Command{label: input[:i], op: "=", value: value})
+		}
 	}
 	return
 }
@@ -28,13 +48,49 @@ func main() {
 	for scanner.Scan() {
 		txt = txt + scanner.Text()
 	}
-
 	inputs := strings.Split(txt, ",")
+	cmds := parseCommands(inputs)
+
+	boxes := make(map[int][]LabelAndValue)
+	for _, cmd := range cmds {
+		if cmd.op == "-" {
+			loc := hash(cmd.label)
+			box, ok := boxes[loc]
+			if !ok {
+				continue
+			}
+			for i, b := range box {
+				if b.label == cmd.label {
+					boxes[loc] = append(box[:i], box[(i+1):]...)
+				}
+			}
+		}
+		if cmd.op == "=" {
+			loc := hash(cmd.label)
+			box, ok := boxes[loc]
+			if !ok {
+				box = []LabelAndValue{}
+			}
+			updated := false
+			for i, b := range box {
+				if b.label == cmd.label {
+					box[i] = LabelAndValue{label: cmd.label, value: cmd.value}
+					updated = true
+					break
+				}
+			}
+			if !updated {
+				box = append(box, LabelAndValue{label: cmd.label, value: cmd.value})
+				boxes[loc] = box
+			}
+		}
+	}
 
 	ret := 0
-	for _, input := range inputs {
-		ret += hash(input)
+	for k, box := range boxes {
+		for i, labelAndVal := range box {
+			ret += (k + 1) * (i + 1) * labelAndVal.value
+		}
 	}
 	println(ret)
-
 }
